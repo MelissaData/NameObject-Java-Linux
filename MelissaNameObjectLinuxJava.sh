1c406eaa03f780d8f1f71f7568b8f213b32a159c
Melissa <>
2023-12-28 09:52:38 -0800

    Tested successfully using Melissa data files for 2023.12

--- MelissaNameObjectLinuxJava.sh
+++ MelissaNameObjectLinuxJava.sh
@@ -43,7 +43,7 @@ done
 
 
 # ######################### Config ###########################
-RELEASE_VERSION='2023.11'
+RELEASE_VERSION='2023.12'
 ProductName="DQ_NAME_DATA"
 
 # Uses the location of the .sh file 
